5eb4ceb8c5baf2a7b601d4b52a82430f9b723878
Valeriy Tyutyunnik <valeriytyutyunnik@yandex.ru>
2020-07-26 19:31:02 +0300

    Create clicker.py

---
+++ clicker.py
@@ -0,0 +1,23 @@
+import pyautogui
+import keyboard
+
+pyautogui.PAUSE = 0.01
+
+def clicker():
+    is_run = False
+    while True:
+      try:
+          if keyboard.is_pressed('F1') and not is_run:
+              is_run = True
+          if keyboard.is_pressed('F2') and is_run:
+              is_run = False
+          if is_run:
+              x, y = pyautogui.position()
+              pyautogui.click(x, y)
+      except KeyboardInterrupt:
+          break
+
+
+if __name__ == '__main__':
+    print('Press F1 to start autoclick. Press F2 to stop it. Ctrl-C to quit')
+    clicker()
